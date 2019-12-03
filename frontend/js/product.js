1c6511164fd1c4699bd079457d4386770d891ad2
Tawinee <fair.tawineesrikarn@gmail.com>
2019-12-03 19:29:21 +0700

    13 submit

--- frontend/js/product.js
+++ frontend/js/product.js
@@ -60,7 +60,7 @@ $(function () {
         $.post(url, {newproduct:txt},function(result){
         $("span").html(result);
         
-    });}
+    });});
         // ===============================
 
     });
